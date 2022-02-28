217361a6a886034455b40599535015f9a42b8662
accrazed <accrazed@gmail.com>
2022-02-27 20:27:02 -0500

    add prettier formatter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>WOO COOKING APP LETS GOOOO</p>
-            </header>
-        </div>
-    );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>WOO COOKING APP LETS GOOOO</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;

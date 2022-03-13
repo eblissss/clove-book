71fc4871eb00006d611be79583fcb6b186d48c38
Kat <70778931+katefort@users.noreply.github.com>
2022-03-13 18:49:12 -0400

    Add route names for requests

--- server/main.go
+++ server/main.go
@@ -22,26 +22,26 @@ func main() {
 	r.GET("/test", routes.Test)
 
 	// Recipes
-	r.POST("/recipes")
-	r.GET("/recipes")
+	r.POST("/recipes", routes.MakeRecipe)
+	r.GET("/recipes", routes.SearchRecipes)
 	r.DELETE("/recipes/:cookbookID", routes.DeleteRecipe)
-	r.PUT("/recipes/:cookbookID")
-	r.POST("/users/:username/favorites")
-	r.GET("/users/:username/favorites")
+	r.PUT("/recipes/:cookbookID", routes.UpdateRecipe)
+	r.POST("/users/:username/favorites", routes.SaveFavorite)
+	r.GET("/users/:username/favorites", routes.ViewFavorites)
 
 	// Ingredients
-	r.GET("/users/:username/ingredients")
-	r.POST("/users/:username/ingredients")
-	r.DELETE("/users/:username/ingredients")
-	r.PUT("/users/:username/ingredients")
+	r.GET("/users/:username/ingredients", routes.ViewIngredients)
+	r.POST("/users/:username/ingredients", routes.AddIngredients)
+	r.DELETE("/users/:username/ingredients", routes.DeleteIngredients)
+	r.PUT("/users/:username/ingredients", routes.UpdateIngredient) // What is the use case of this?
 
 	// Users
 	r.POST("/users", routes.MakeUser)
-	r.GET("/users/login")
-	r.GET("/users/logout")
-	r.GET("/users/:username")
-	r.PUT("/users/:username")
-	r.DELETE("/users/:username")
+	r.GET("/users/login", routes.LogIn)
+	r.GET("/users/logout", routes.LogOut)
+	r.GET("/users/:username", routes.GetUser)
+	r.PUT("/users/:username", routes.UpdateUsername)
+	r.DELETE("/users/:username", routes.DeleteUser)
 
 	//this runs the server and allows it to listen to requests.
 	r.Run(":" + port)
--- server/models/recipe.go
+++ server/models/recipe.go
@@ -1,5 +1,22 @@
 package models
 
-type Recipe struct{}
+type Recipe struct {
+	RecipeName 	string 		`json:"name"`
+	Author 		string 		`json:"author"`
+	CookTime 	int64 		`json:"cookTime"`
+	PrepTime 	int64 		`json:"prepTime"`
+	TotalTime 	int64 		`json:"totalTime"`
+	Tags 		[]string 	`json:"tags"`
+	Ingredients []string 	`json:"ingredients"`
+}
 
-type RecipeStub struct{}
+// Recipestub for both user-created and other recipes
+type RecipeStub struct {
+	RecipeId 		int			`json:""`
+	RecipeName 		string 		`json:"name"`
+	IsUserRecipe 	bool 		`json:"userRecipe"`
+	IsMyRecipe 		bool 		`json:"myRecipe"`
+	TotalTime 		int64 		`json:"totalTime"`
+	Tags 			[]string 	`json:"tags"`
+	Ingredients 	[]string 	`json:"ingredients"` // not sure about inclusion
+}

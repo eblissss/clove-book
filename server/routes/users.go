44988b758f7c572b9479e93240ba6cbfbb166d45
accrazed <accrazed@gmail.com>
2022-03-14 21:13:23 -0400

    add server email client

--- server/routes/users.go
+++ server/routes/users.go
@@ -3,7 +3,10 @@ package routes
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"net/smtp"
+	"os"
 	"server/models"
 	"time"
 
@@ -16,39 +19,67 @@ import (
 
 var validate = validator.New()
 var userCollection *mongo.Collection = OpenCollection(Client, "users")
+var pendingUserCollection *mongo.Collection = OpenCollection(Client, "pending_users")
 
-func MakeUser(c *gin.Context) {
+func AuthUser(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 	defer cancel()
 
-	newUser := &models.User{}
-
-	if err := c.BindJSON(&newUser); err != nil {
+	// Unwrap request into AuthUser
+	user := &models.AuthUser{}
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	if err := validate.Struct(user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		fmt.Println(err)
 		return
 	}
 
-	if err := validate.Struct(newUser); err != nil {
+	// TODO: check email valid
+	// TODO: check username valid
+
+	
+
+	fmt.Println("Email sent successfully")
+
+}
+
+func RegisterUser(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	// Unwrap request into user
+	user := &models.User{}
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	if err := validate.Struct(user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		fmt.Println(err)
 		return
 	}
 
+	// Check for taken username
 	if res := userCollection.FindOne(ctx, bson.M{
-		"username": newUser.Username,
+		"username": user.Username,
 	}); res.Err() == nil {
-		err := fmt.Sprintf("username taken: %s", newUser.Username)
+		err := fmt.Sprintf("username taken: %s", user.Username)
 		c.JSON(http.StatusConflict, gin.H{"error": err})
 		fmt.Println(err)
 		return
 	}
 
-	newUser.UserID = primitive.NewObjectID()
-	newUser.CreatedAt = time.Now().String()
-	newUser.UpdatedAt = time.Now().String()
+	user.UserID = primitive.NewObjectID()
+	user.CreatedAt = time.Now().String()
+	user.UpdatedAt = time.Now().String()
 
-	result, err := userCollection.InsertOne(ctx, *newUser)
+	// Insert user
+	result, err := userCollection.InsertOne(ctx, *user)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "user was not created"})
 		fmt.Println(err)
@@ -61,26 +92,24 @@ func MakeUser(c *gin.Context) {
 }
 
 func LogIn(c *gin.Context) {
-	
-	// ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
-	// defer cancel()
+	// 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	// 	defer cancel()
 
-	// user := &models.User{}
-	// un = c.Params.ByName("username")
-	// pw = c.Params.ByName("password")
+	// 	user := &models.User{}
+	// 	un := c.Params.ByName("username")
+	// 	pw := c.Params.ByName("password")
 
-	// err := userCollection.Find(ctx, bson.M{
-	// 	"username": un,
-	// 	"password": pw
-	// }).Decode(&user)
+	// 	err := userCollection.Find(ctx, bson.M{
+	// 		"username": un,
+	// 		"password": pw,
+	// 	}).Decode(&user)
 
-	// if err.Err() != nil {
-	// 	err := fmt.Sprintf("Username not found: %s", Username)
-	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-	// 	fmt.Println(err)
-	// 	return
-	// }
+	// 	if err.Err() != nil {
+	// 		err := fmt.Sprintf("Username not found: %s", Username)
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
 
-	c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: M.{"data": user}})
+	// 	c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: gin.H{"data": user}})
 }
-

d241faef46ea3741934970ff7111631f5b1823a2
CathyChian <chiancathy@gmail.com>
2022-03-23 16:38:36 -0400

    instructions kinda

--- server/models/recipe.go
+++ server/models/recipe.go
@@ -3,14 +3,14 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Recipe struct {
-	RecipeName  string       `json:"name"`
-	Author      string       `json:"author"`
-	CookTime    int64        `json:"cookTime"`
-	PrepTime    int64        `json:"prepTime"`
-	TotalTime   int64        `json:"totalTime"`
-	Tags        []string     `json:"tags"`
-	Ingredients []Ingredient `json:"ingredients"`
-	// TODO: add instructions
+	RecipeName   string        `json:"name"`
+	Author       string        `json:"author"`
+	CookTime     int64         `json:"cookTime"`
+	PrepTime     int64         `json:"prepTime"`
+	TotalTime    int64         `json:"totalTime"`
+	Tags         []string      `json:"tags"`
+	Ingredients  []Ingredient  `json:"ingredients"`
+	Instructions []Instruction `json:"instructions"`
 }
 
 // Recipestub for both user-created and other recipes
@@ -28,4 +28,10 @@ type Ingredient struct {
 	IngredientName string  `json:"name"`
 	Amount         float64 `json:"amount"`
 	Unit           string  `json:"unit"`
+}
+
+type Instruction struct {
+	Number      int64  `json:"number"` // not sure this is necessary when instructions are in an array and have an index?
+	Description string `json:"description"`
+	// TODO: add array of ingredients?
 }

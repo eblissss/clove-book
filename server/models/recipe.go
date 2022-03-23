9e2241c00711460478dac73a9d786d22c4471d82
Kat <70778931+katefort@users.noreply.github.com>
2022-03-22 22:11:25 -0400

    uh idk

--- server/models/recipe.go
+++ server/models/recipe.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Recipe struct {
 	RecipeName   string   `json:"name"`
-	Author       string   `json:"author"`
+	Author         string     `json:"author"`
 	CookTime     int64    `json:"cookTime"`
 	PrepTime     int64    `json:"prepTime"`
 	TotalTime    int64    `json:"totalTime"`
@@ -16,9 +16,9 @@ type Recipe struct {
 type RecipeStub struct {
 	CookbookID    primitive.ObjectID `json:"cookbookID" bson:"cookbookID"`
 	SpoonacularID int                `json:"spoonacularID" bson:"spoonacularID"`
-	RecipeName    string             `json:"name"`
-	IsUserRecipe  bool               `json:"userRecipe"`
-	TotalTime     int64              `json:"totalTime"`
-	Tags          []string           `json:"tags"`
-	Ingredients   []string           `json:"ingredients"` // not sure about inclusion
+	RecipeName    string             `json:"name" bson:"name"`
+	IsUserRecipe  bool               `json:"userRecipe" bson:"userRecipe"`
+	TotalTime     int64              `json:"totalTime" bson:"totalTime"`
+	Tags          []string           `json:"tags" bson:"tags"`
+	Ingredients   []string           `json:"ingredients" bson:"ingredients"` // not sure about inclusion
 }

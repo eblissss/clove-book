175f883b542e76399594be21f5d13dcac92d1b64
CathyChian <chiancathy@gmail.com>
2022-03-23 15:45:54 -0400

    ingredients now json array

--- server/models/recipe.go
+++ server/models/recipe.go
@@ -3,23 +3,29 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Recipe struct {
-	RecipeName  string   `json:"name"`
-	Author      string   `json:"author"`
-	CookTime    int64    `json:"cookTime"`
-	PrepTime    int64    `json:"prepTime"`
-	TotalTime   int64    `json:"totalTime"`
-	Tags        []string `json:"tags"`
-	Ingredients []string `json:"ingredients"` // TODO: change to array of json objects to include quantities
+	RecipeName  string       `json:"name"`
+	Author      string       `json:"author"`
+	CookTime    int64        `json:"cookTime"`
+	PrepTime    int64        `json:"prepTime"`
+	TotalTime   int64        `json:"totalTime"`
+	Tags        []string     `json:"tags"`
+	Ingredients []Ingredient `json:"ingredients"`
 	// TODO: add instructions
 }
 
 // Recipestub for both user-created and other recipes
 type RecipeStub struct {
 	CookbookID    primitive.ObjectID `json:"cookbookID" bson:"cookbookID"`
-	SpoonacularID int                `json:"spoonacularID" bson:"spoonacularID"`
+	SpoonacularID int64              `json:"spoonacularID" bson:"spoonacularID"`
 	RecipeName    string             `json:"name" bson:"name"`
 	IsUserRecipe  bool               `json:"userRecipe" bson:"userRecipe"`
 	TotalTime     int64              `json:"totalTime" bson:"totalTime"`
 	Tags          []string           `json:"tags" bson:"tags"`
-	Ingredients   []string           `json:"ingredients" bson:"ingredients"` // not sure about inclusion
+	Ingredients   []Ingredient       `json:"ingredients" bson:"ingredients"` // not sure about inclusion
+}
+
+type Ingredient struct {
+	IngredientName string  `json:"name"`
+	Amount         float64 `json:"amount"`
+	Unit           string  `json:"unit"`
 }
